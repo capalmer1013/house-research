b1f57e1d2eeabf54d8cc121b3ffba5ec1f166be2
Chris Palmer <capalmer1013@gmail.com>
2023-02-17 01:45:45 -0500

    displaying a graph alright

--- main.py
+++ main.py
@@ -24,10 +24,12 @@ columns = [
 houses = pd.read_csv(csv_filename)
 
 # houses["SOLD DATE 2"] = pd.to_datetime(houses["SOLD DATE"], format="%B-%d-%Y")
-houses["new_sold_date"] = pd.to_datetime(houses["SOLD DATE"], format="%B-%d-%Y").dt.date
+houses["DATE SOLD"] = pd.to_datetime(houses["SOLD DATE"], format="%B-%d-%Y")
 # houses["new_sold_date"] = dt.datetime.strptime(houses["SOLD DATE"], "%B-%d-%Y").date()
-for each in houses['new_sold_date']:
-    print(each)
+# for each in houses["new_sold_date"]:
+#     print(each)
+
+print(houses.info())
 
 @app.command()
 def test():
@@ -37,12 +39,28 @@ def test():
     print("===================")
     print(houses[columns].info())
 
+
 @app.command()
 def plot():
-    # houses.plot.scatter(x="SQUARE FEET", y="PRICE", c="")
-    #houses
-    scatter_matrix(houses[["SQUARE FEET", "PRICE", "BEDS", "BATHS", "new_sold_date"]], figsize=(10, 10), diagonal="kde")
+    houses.plot.scatter(x="DATE SOLD", y="DOLLAR PER SQUARE FEET", c="PRICE")
+    plt.gcf().autofmt_xdate()
+    # houses
+    # scatter_matrix(
+    #     houses[
+    #         [
+    #             "SQUARE FEET",
+    #             "PRICE",
+    #             "LOT SIZE",
+    #             "YEAR BUILT",
+    #             "DOLLAR PER SQUARE FEET",
+    #             # "new_sold_date",
+    #         ]
+    #     ],
+    #     figsize=(10, 10),
+    #     diagonal="kde",
+    # )
     plt.show()
+
 
 if __name__ == "__main__":
     app()
