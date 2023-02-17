7193eb403260550282093760eefb1e0b062c660a
Chris Palmer <capalmer1013@gmail.com>
2023-02-16 23:12:32 -0500

    formatted and added typer

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
 import pandas as pd
 
 csv_filename = "redfin_2023-02-16-16-44-41.csv"
-columns = ["SOLD DATE", "PROPERTY TYPE", "ADDRESS", "CITY", "PRICE", "BEDS", "BATHS", "SQUARE FEET", "LOT SIZE", "YEAR BUILT", "DAYS ON MARKET", "$/SQUARE FEET", "HOA/MONTH","URL"]
+columns = [
+    "SOLD DATE",
+    "PROPERTY TYPE",
+    "ADDRESS",
+    "CITY",
+    "PRICE",
+    "BEDS",
+    "BATHS",
+    "SQUARE FEET",
+    "LOT SIZE",
+    "YEAR BUILT",
+    "DAYS ON MARKET",
+    "DOLLAR PER SQUARE FEET",
+    "HOA/MONTH",
+    "URL",
+]
 houses = pd.read_csv(csv_filename)
 print(houses)
 print(houses.dtypes)
 print(houses.info())
+print("===================")
+print(houses[columns].info())
